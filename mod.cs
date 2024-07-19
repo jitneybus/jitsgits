d827b5c98c485c3ce44d7a71ad0bce8f8a4586b5
jitneybus <149901966+jitneybus@users.noreply.github.com>
2024-07-18 23:42:38 -0400

    Update mod.cs

--- mod.cs
+++ mod.cs
@@ -1,5 +1,5 @@
 /**
-* <author>Kelrae and Jitney</author>
+* <author>Kelrae and Jitneybus</author>
 * <email>Kelrae.Vante@gmail.com</email>
 * <url>RPCLIF.com</url>
 * <credits>Kelrae, Jitney</credits>
@@ -30,7 +30,27 @@ package RPCLiFCanonRecipes
     function RPCLiFCanonRecipes::dbChanges() {
            //// Update Recipes to match canon lore ////
       dbi.Update("UPDATE `objects_types` SET `name`='Kheldir\'s Cobbler' WHERE ID='986'"); // Confiture Object
-      dbi.Update("UPDATE `recipe` SET `name`='Idol of Betong' WHERE ID='481'"); // Confiture Recipe
+      dbi.Update("UPDATE `recipe` SET `name`='Kheldir\'s Cobbler' WHERE ID='481'"); // Confiture Recipe
+	  dbi.Update("UPDATE `objects_types` SET `name`='Cavalier\'s Carnage Kabobs' WHERE ID='1021'"); // Meat Kabobs Object
+      dbi.Update("UPDATE `recipe` SET `name`='Cavalier\'s Carnage Kabobs' WHERE ID='519'"); // Meat Kabobs Recipe
+	  dbi.Update("UPDATE `objects_types` SET `name`='Siraye Sushi' WHERE ID='980'"); // Rollmops Object
+      dbi.Update("UPDATE `recipe` SET `name`='Siraye Sushi' WHERE ID='478'"); // Rollmops Recipe
+	  dbi.Update("UPDATE `objects_types` SET `name`='Crescian Beefcake' WHERE ID='1004'"); // Yorkshire Pudding Object
+      dbi.Update("UPDATE `recipe` SET `name`='Crescian Beefcake' WHERE ID='503'"); // Yorkshire Pudding Recipe
+	  dbi.Update("UPDATE `objects_types` SET `name`='Fayeth\'s Nectar' WHERE ID='981'"); // Mulled Wine Object
+      dbi.Update("UPDATE `recipe` SET `name`='Fayeth\'s Nectar' WHERE ID='485'"); // Mulled Wine Recipe
+	  dbi.Update("UPDATE `objects_types` SET `name`='Queen\'s Buns' WHERE ID='978'"); // Cheese buns Object
+      dbi.Update("UPDATE `recipe` SET `name`='Queen\'s Buns' WHERE ID='468'"); // Cheese buns Recipe
+	  dbi.Update("UPDATE `objects_types` SET `name`='Prisoner\'s Mead' WHERE ID='966'"); // Compote Object
+      dbi.Update("UPDATE `recipe` SET `name`='Prisoner\'s Mead' WHERE ID='461'"); // Compote Recipe
+	  dbi.Update("UPDATE `objects_types` SET `name`='Chieftain\'s Feast' WHERE ID='1020'"); // Waterzooi Object
+      dbi.Update("UPDATE `recipe` SET `name`='Chieftain\'s Feast' WHERE ID='518'"); // Waterzooi Recipe
+	  dbi.Update("UPDATE `objects_types` SET `name`='Rocky Bread' WHERE ID='976'"); // Samsa Object
+      dbi.Update("UPDATE `recipe` SET `name`='Rocky Bread' WHERE ID='467'"); // Samsa Recipe
+	  dbi.Update("UPDATE `objects_types` SET `name`='Iskar\'s Demise' WHERE ID='968'"); // Cake with Giblets Object
+      dbi.Update("UPDATE `recipe` SET `name`='Iskar\'s Demise' WHERE ID='465'"); // Cake with Giblets Recipe
+	  dbi.Update("UPDATE `objects_types` SET `name`='Tea' WHERE ID='982'"); // Eggnog Object
+      dbi.Update("UPDATE `recipe` SET `name`='Tea' WHERE ID='479'"); // Eggnog Recipe
 
     }
 
